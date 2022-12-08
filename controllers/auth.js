68c7eb50ab902481761c9d784f3efa2fe47fda23
alialaba <alialaba079@gmail.com>
2022-12-08 17:20:49 -0500

    chores: install bcrypt and ft: implement signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,18 @@
-exports.signup =(req,res,next)=>{
-res.send("Signup route")
+const User = require("../models/userModel");
+
+exports.signup  = async (req,res,next)=>{
+
+const {firstname, lastname, username, password, email} =  req.body;
+
+try {
+    const user = await User.create({firstname, lastname, username, password, email});
+
+    return res.status(201).json({success: true, user:user });
+} catch (error) {
+    
+    return res.status(500).json({success:false, error: error.message});
+}
+
 }
 
 exports.login = (req,res,next)=>{
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt")
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -27,13 +28,34 @@ const UserSchema = new Schema({
         select: false
     },
     resetPasswordToken: String,
-    resetPasswordExpired: string
-
-       
-    
+    resetPasswordExpired: String 
 })
 
 
-const User = mongoose.model("users", UserSchema)
+
+// The code in the UserScheme.pre() function is called a pre-hook.
+// Before the user information is saved in the database, this function will be called,
+// you will get the plain text password, hash it, and store it.
+UserSchema.pre(
+    'save',
+    async function (next) {
+        const user = this;
+        const hash = await bcrypt.hash(this.password, 10);
+
+        this.password = hash;
+        next();
+    }
+);
+
+// You will also need to make sure that the user trying to log in has the correct credentials. Add the following new method:
+UserSchema.methods.isValidPassword = async function(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+  
+    return compare;
+  }
+
+
+const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
